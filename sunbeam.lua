c4054079f7b704190a2acab1647a53b7c25020fd
Timon Walshe-Grey <me@timon.red>
2017-12-14 19:22:00 +0000

    Abstract out concept of "reserved properties"

--- sunbeam.lua
+++ sunbeam.lua
@@ -1,10 +1,53 @@
+local symbols = {
+	prototype = {},
+	constructor = {}
+}
+
+function symbols.prototype:get()
+	return self[symbols.prototype]
+end
+function symbols.prototype:set(value)
+	if rawget(self, symbols.prototype) then
+		error("cannot reassign an object's prototype")
+	else
+		self[symbols.prototype] = value
+	end
+end
+
+function symbols.constructor:get()
+	if rawget(self, symbols.constructor) then
+		return self[symbols.constructor]
+	elseif self.prototype ~= self then
+		return self.prototype.constructor
+	else
+		return function() end
+	end
+end
+function symbols.constructor:set(value)
+	if type(value) == 'function' or getmetatable(value).__call then
+		self[symbols.constructor] = value
+	else
+		error("an object's constructor must be callable")
+	end
+end
+
 local metatable = {}
 
 function metatable:__index(key)
-	if key == 'prototype' then
-		return nil
+	if symbols[key] then
+		return symbols[key].get(self)
 	elseif self.prototype ~= self then
 		return self.prototype[key]
+	else
+		return nil
+	end
+end
+
+function metatable:__newindex(key, value)
+	if symbols[key] then
+		symbols[key].set(self, value)
+	else
+		rawset(self, key, value)
 	end
 end
 
@@ -12,16 +55,15 @@ function metatable:__call(...)
 	local instance = {}
 	setmetatable(instance, metatable)
 	instance.prototype = self
-	self.constructor(instance, ...)
+	instance:constructor(...)
 	return instance
 end
 
-
-function sunbeam()
+local function sunbeam(...)
 	local instance = {}
 	setmetatable(instance, metatable)
 	instance.prototype = instance
-	instance.constructor = function() end
+	instance:constructor(...)
 	return instance
 end
 
