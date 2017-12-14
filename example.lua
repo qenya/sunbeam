05313419fa76d41c8fc04614a301bcd5d4688780
Timon Walshe-Grey <me@timon.red>
2017-12-14 20:05:40 +0000

    Improve examples

--- example.lua
+++ example.lua
@@ -1,29 +1,41 @@
 local sunbeam = require 'sunbeam'
 
+-- Create a base object by calling sunbeam().
 local fruit = sunbeam()
+print(fruit.prototype == fruit) -- true
+
+-- Instantiate objects by calling them.
+local apple = fruit()
+print(apple.prototype == fruit) -- true
+
+-- Give an object a constructor and new instances will call it.
 function fruit:constructor(color)
 	self.color = color
 end
+apple = fruit("red")
+print(apple.color) -- red
 
+-- Instances inherit properties from their prototypes.
+fruit.niceDessert = true
+print(apple.niceDessert) -- true
+
+-- But you can change them without affecting the prototype.
 local tomato = fruit("red")
-print(fruit.color) -- nil
-print(tomato.color) -- "red"
+tomato.niceDessert = false
+print(fruit.niceDessert) -- true
+print(apple.niceDessert) -- true
+print(tomato.niceDessert) -- false
 
-fruit.sweetness = 7
-tomato.sweetness = tomato.sweetness - 5
-print(fruit.sweetness) -- 7
-print(tomato.sweetness) -- 2
+-- By default, constructors are also inherited.
+local greenzebra = tomato("green")
+print(tomato.color) -- red
+print(greenzebra.color) -- green
 
-print(tomato.prototype == fruit) -- true
-
-
-local peartomato = tomato("yellow")
-print(peartomato.color) -- yellow
-
---[[
-function tomato:constructor(color)
-	super(color) -- TODO: Decide syntax
+-- But you can also call a superconstructor manually.
+function tomato:constructor(shape, color)
+	tomato.prototype.constructor(self, color)
+	self.shape = shape
 end
-local peartomato = tomato("yellow")
-print(peartomato.color) -- yellow
-]]
+local cornseed = tomato("pearshaped", "yellow")
+print(cornseed.shape) -- pearshaped
+print(cornseed.color) -- yellow
